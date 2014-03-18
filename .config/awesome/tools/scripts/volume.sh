ecfb2a46ece82967c51a73202e31350028585c75
mr.Shu <mr@shu.io>
2014-03-19 00:18:19 +0100

    headphones and master and stuff

--- .config/awesome/tools/scripts/volume.sh
+++ .config/awesome/tools/scripts/volume.sh
@@ -2,7 +2,16 @@
 #
 #amixer get Master | grep "Front Left:" | awk '{print $5}' > /tmp/vollevel.out
 #RES=$( cat /tmp/vollevel.out )
-RES=$( amixer get $1 | grep "Front Left:" | awk '{print $5}' )
+cat /proc/asound/card0/codec\#0 | awk '/Speaker Phantom Jack/,/Power/'| grep 'Pin-ctls: .* OUT' > /dev/null
+
+# Headphones detection
+#if [[ $? -eq 0 ]]; then
+#        RES=$( amixer get Master | grep "Mono:" | awk '{print $4}' )
+#else
+#        RES=$( amixer get Headphone | grep "Front Left:" | awk '{print $5}' )
+#fi
+
+RES=$( amixer get Master | grep "Mono:" | awk '{print $4}' )
 #echo -e $RES
 #RES="[30%]"
 RES=${RES##"["}
