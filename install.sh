98bf10124df3b98106b347b6a663b0fc0e0333fe
mr.Shu <mr@shu.io>
2014-06-20 21:06:16 +0200

    updated install script

--- install.sh
+++ install.sh
@@ -2,9 +2,11 @@
 
 echo "-> Installation of mrShu's dotfiles started"
 
-for x in $(find . -path ./.git -prune -o -print) do
-        echo "Installing $x into ~/"
-        cp -R ./$x ~/.$x
+for x in $(find . -path ./.git -prune -o -print); do
+        [[ "$x" == "." ]] && continue
+
+        echo "Installing $x into ~/.$(echo $x|sed 's#./##')"
+        cp -R ./$x ~/.$(echo $x|sed 's#./##')
 done
 
 mkdir -p ~/.vim/backup
